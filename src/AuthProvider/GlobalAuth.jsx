4d6cde4e65896c57e65c9306d485ea4f3d4dc452
Biddut-Roy <biddut.chandro.roy2@gmail.com>
2023-12-20 12:56:42 +0600

    Global auth section complete

---
+++ src/AuthProvider/GlobalAuth.jsx
@@ -0,0 +1,59 @@
+import { GoogleAuthProvider, createUserWithEmailAndPassword, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, signOut } from "firebase/auth";
+import { createContext, useEffect, useState } from "react";
+import PropTypes from 'prop-types'; 
+import auth from "../Firebase/Firebase.int";
+
+
+
+export const AuthContext = createContext()
+
+const GlobalAuth =({children})=> {
+    const [user, setUser] = useState(null)
+    const [loader, setLoader] = useState(true)
+    const Provider = new GoogleAuthProvider();
+
+    const createUser = (email, password) => {
+        setLoader(true);
+        return createUserWithEmailAndPassword(auth, email, password);
+    }
+
+    const signInGoogle = () => {
+        setLoader(true);
+        return signInWithPopup(auth, Provider);
+    }
+    
+    const login = (email, password) => {
+        setLoader(true);
+        return signInWithEmailAndPassword(auth, email, password);
+
+    }
+    const logOut = () => {
+        setLoader(true);
+        return signOut(auth);
+    }
+
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+            setUser(currentUser);
+            setLoader(false)
+        })
+        return () => {
+            unsubscribe();
+        }
+    }, [user?.email])
+
+
+    const info = { user , loader , createUser , signInGoogle , login , logOut}
+
+    return (
+        <AuthContext.Provider value={info}>
+            {children}
+        </AuthContext.Provider>
+    );
+};
+
+GlobalAuth.propTypes ={
+    children: PropTypes.node,
+}
+
+export default GlobalAuth;
--- src/main.jsx
+++ src/main.jsx
@@ -4,13 +4,16 @@ import './index.css'
 import { RouterProvider } from 'react-router-dom'
 import { router } from './Route/Route'
 import { HelmetProvider } from 'react-helmet-async';
+import GlobalAuth from './AuthProvider/GlobalAuth'
 
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <HelmetProvider>
-      <RouterProvider router={router} />
-    </HelmetProvider>
+    <GlobalAuth>
+      <HelmetProvider>
+        <RouterProvider router={router} />
+      </HelmetProvider>
+    </GlobalAuth>
   </React.StrictMode>,
 )
